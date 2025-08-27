0a1f0812f54c70bc7126d8b6aadb3de16b7f1d6e
freitagb <brian.freitag@nsstc.uah.edu>
2025-08-27 13:29:38 -0500

    update about content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,9 @@ export default function Page() {
   return (
     <div className='grid-container'>
       <p>
-        Content describing your program and instance of the VEDA Dashboard will go here. It is fully configurable to include graphics and links, similar to other pages.
+        The NASA Disasters Program promotes the use of Earth observations to reduce risk, improve response, hasten recovery and promote resilience from natural and technological disasters on local and global scales. By sponsoring application science and fostering domestic and international partnerships, the program seeks to enable resilient communities in advance of disasters, and use Earth observation data to aid decision-making when disasters strike.
+
+        The program targets a broad spectrum of hazards and disasters, including floods, hurricanes, earthquakes, volcanoes and landslides, as well as combined hazards and cascading impacts. When disasters impact communities, the program collects and distributes relevant imagery, data, and damage assessments to provide response agencies and local governments with actionable data to aid in planning and recovery. The Disasters Program and its network of partners and volunteers assist with hazard assessment, evaluating severity, and identifying impacts near vulnerable infrastructure, crops, and lifelines, especially in remote areas where observations are sparse.
       </p>
     </div>
   );
