5613156fc28d19c00d48e335aaf40a96325dc409
freitagb <brian.freitag@nsstc.uah.edu>
2025-08-27 14:01:33 -0500

    testing content changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,9 +4,9 @@ export default function Page() {
   return (
     <div className='grid-container'>
       <p>
-        The NASA Disasters Program promotes the use of Earth observations to reduce risk, improve response, hasten recovery and promote resilience from natural and technological disasters on local and global scales. By sponsoring application science and fostering domestic and international partnerships, the program seeks to enable resilient communities in advance of disasters, and use Earth observation data to aid decision-making when disasters strike.<br>
-        The program targets a broad spectrum of hazards and disasters, including floods, hurricanes, earthquakes, volcanoes and landslides, as well as combined hazards and cascading impacts. When disasters impact communities, the program collects and distributes relevant imagery, data, and damage assessments to provide response agencies and local governments with actionable data to aid in planning and recovery. The Disasters Program and its network of partners and volunteers assist with hazard assessment, evaluating severity, and identifying impacts near vulnerable infrastructure, crops, and lifelines, especially in remote areas where observations are sparse.<br>
-        <a href='https://appliedsciences.nasa.gov/what-we-do/disasters'> Learn More</a>.
+        The NASA Disasters Program promotes the use of Earth observations to reduce risk, improve response, hasten recovery and promote resilience from natural and technological disasters on local and global scales. By sponsoring application science and fostering domestic and international partnerships, the program seeks to enable resilient communities in advance of disasters, and use Earth observation data to aid decision-making when disasters strike.
+        The program targets a broad spectrum of hazards and disasters, including floods, hurricanes, earthquakes, volcanoes and landslides, as well as combined hazards and cascading impacts. When disasters impact communities, the program collects and distributes relevant imagery, data, and damage assessments to provide response agencies and local governments with actionable data to aid in planning and recovery. The Disasters Program and its network of partners and volunteers assist with hazard assessment, evaluating severity, and identifying impacts near vulnerable infrastructure, crops, and lifelines, especially in remote areas where observations are sparse.
+        <a href='https://appliedsciences.nasa.gov/what-we-do/disasters'> Learn More</a>
 
       </p>
     </div>
