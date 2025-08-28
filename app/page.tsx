3b3aba6180ca8280a5d3673c3996e394eefe8668
freitagb <brian.freitag@nsstc.uah.edu>
2025-08-28 11:53:58 -0500

    add website maintenance banner

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,14 @@ const otherStories = featuredStories.slice(1);
 export default function HomePage() {
   return (
     <section>
+      <div class='usa-alert usa-alert--error' role='alert'>
+        <div class='usa-alert__body'>
+          <h4 class='usa-alert__heading'>Error status</h4>
+          <p class='usa-alert__text'>
+          This website is currently under maintenance. Some services and webpages may not work as expected.
+          </p>
+        </div>
+      </div>
       <div className='usa-alert usa-alert--info'>
         <div className='usa-alert__body'>
          <h4 className='usa-alert__heading'>Current Activations</h4>
