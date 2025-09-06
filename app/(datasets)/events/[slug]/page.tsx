5fe938f95d70c25043cd48aa1130d0c654dc3998
freitagb <brian.freitag@nsstc.uah.edu>
2025-09-05 22:19:18 -0500

    update site routing

--- app/(datasets)/events/[slug]/page.tsx
+++ app/(datasets)/events/[slug]/page.tsx
@@ -1,52 +1,30 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
 import { CustomMDX } from 'app/components/mdx';
-import { getStories } from 'app/content/utils/mdx';
-import { LegacyGlobalStyles, PageHero } from '@lib';
-import './index.scss';
+import { getDatasets } from 'app/content/utils/mdx';
+import { PageHero, LegacyGlobalStyles } from '@lib';
 import Providers from 'app/(datasets)/providers';
 
-async function generateStaticParams() {
-  const posts = getStories();
+export default function DatasetOverviewPage({ params }: { params: any }) {
+  const dataset = getDatasets().find((dataset) => dataset.slug === params.slug);
 
-  return posts.map((post) => ({ slug: post.slug }));
-}
-
-export default function StoryOverview({ params }: { params: any }) {
-  const post = getStories().find((post) => post.slug === params.slug);
-
-  if (!post) {
+  if (!dataset) {
     notFound();
   }
 
   return (
     <section>
-      <script
-        type='application/ld+json'
-        suppressHydrationWarning
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'Story',
-            title: post.metadata.name,
-            description: post.metadata.description,
-            coverSrc: post.metadata.media?.src,
-            coverAlt: post.metadata.media?.alt,
-          }),
-        }}
-      />
       <article className='prose'>
         <Providers>
           <LegacyGlobalStyles />
-
           <PageHero
-            title={post.metadata.name}
-            description={post.metadata.description}
-            coverSrc={post.metadata.media?.src}
-            coverAlt={post.metadata.media?.alt}
+            title={dataset.metadata.name}
+            description={dataset.metadata.description}
+            coverSrc={dataset.metadata.media?.src}
+            coverAlt={dataset.metadata.media?.alt}
           />
-          <CustomMDX source={post.content} />
         </Providers>
+        <CustomMDX source={dataset.content} />
       </article>
     </section>
   );
