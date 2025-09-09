f07e75f39c7ef93552637effba2bb2c68986b3bb
Andrew Blackford <62357205+acblackford@users.noreply.github.com>
2025-09-09 11:37:10 -0500

    update activation banner

--- app/page.tsx
+++ app/page.tsx
@@ -25,11 +25,16 @@ export default function HomePage() {
       </div>
       <div className='usa-alert usa-alert--info'>
         <div className='usa-alert__body'>
-         <h4 className='usa-alert__heading'>Current Activations</h4>
+          <h4 className='usa-alert__heading'>Current Activations</h4>
           <p className='usa-alert__text'>
-           The Disasters Program is currently providing support for the
-           <a className='usa-link' href='https://disasters.openveda.cloud/events/ca-wildfire-2025'>California Wildfires</a> 
-          affecting the Los Angeles metropolitan area.
+            The Disasters Program is currently providing support for the{' '}
+            <a
+              className='usa-link'
+              href='https://deploy-preview-21--disasters-hub.netlify.app/events/ca-wildfires-2025'
+            >
+              California Wildfires
+            </a>{' '}
+            affecting the Los Angeles metropolitan area.
           </p>
         </div>
       </div>
