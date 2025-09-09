1bf5cf3affb578140432046d3a2e05d983d0817c
Andrew Blackford <62357205+acblackford@users.noreply.github.com>
2025-09-09 11:43:49 -0500

    push maintenance banner to all pages

--- app/page.tsx
+++ app/page.tsx
@@ -15,22 +15,14 @@ const otherStories = featuredStories.slice(1);
 export default function HomePage() {
   return (
     <section>
-      <div className='usa-alert usa-alert--error' role='alert'>
-        <div className='usa-alert__body'>
-          <h4 className='usa-alert__heading'>Under Maintenance</h4>
-          <p className='usa-alert__text'>
-          This website is currently being migrated. Some services and webpages may not work as expected.
-          </p>
-        </div>
-      </div>
-      <div className='usa-alert usa-alert--info'>
-        <div className='usa-alert__body'>
-          <h4 className='usa-alert__heading'>Current Activations</h4>
-          <p className='usa-alert__text'>
+      <div className="usa-alert usa-alert--info">
+        <div className="usa-alert__body">
+          <h4 className="usa-alert__heading">Current Activations</h4>
+          <p className="usa-alert__text">
             The Disasters Program is currently providing support for the{' '}
             <a
-              className='usa-link'
-              href='https://deploy-preview-21--disasters-hub.netlify.app/events/ca-wildfires-2025'
+              className="usa-link"
+              href="https://deploy-preview-21--disasters-hub.netlify.app/events/ca-wildfires-2025"
             >
               California Wildfires
             </a>{' '}
@@ -38,48 +30,55 @@ export default function HomePage() {
           </p>
         </div>
       </div>
-      <div className='grid-container'>
-        <div className='grid-row margin-top-5'>
+
+      <div className="grid-container">
+        <div className="grid-row margin-top-5">
           <h4>
-          A powerful interface for viewing, analyzing, and downloading the latest near real-time and disaster specific data products in Geographic Information Systems (GIS) format. The Disasters Mapping Portal supports NASA’s Earth Applied Sciences Disasters program area in its mission to use Earth-observing data and applied research to improve the prediction of, preparation for, response to and recovery from hazards and disasters around the world.
+            A powerful interface for viewing, analyzing, and downloading the
+            latest near real-time and disaster specific data products in
+            Geographic Information Systems (GIS) format. The Disasters Mapping
+            Portal supports NASA’s Earth Applied Sciences Disasters program area
+            in its mission to use Earth-observing data and applied research to
+            improve the prediction of, preparation for, response to and recovery
+            from hazards and disasters around the world.
           </h4>
         </div>
 
-        <div className='grid-row margin-top-5'>
+        <div className="grid-row margin-top-5">
           <h2>Featured Events</h2>
         </div>
-        <div className='grid-row grid-gap-md  margin-top-2'>
-          <div className='tablet:grid-col-6'>
+
+        <div className="grid-row grid-gap-md margin-top-2">
+          <div className="tablet:grid-col-6">
             <div
-              className='card--homepage-topstory text-base-lightest radius-md display-flex flex-align-end padding-2'
+              className="card--homepage-topstory text-base-lightest radius-md display-flex flex-align-end padding-2"
               style={{ backgroundImage: `url(${topStory.media?.src})` }}
             >
-              <div className='card--homepage-topstory-text'>
-                <h3> {topStory.name}</h3>
-                <p className='margin-top-1'> {topStory.description}</p>
+              <div className="card--homepage-topstory-text">
+                <h3>{topStory.name}</h3>
+                <p className="margin-top-1">{topStory.description}</p>
               </div>
-              <Link className='link--block' href={topStory.path} />
+              <Link className="link--block" href={topStory.path} />
             </div>
           </div>
-          <div className='tablet:grid-col-6'>
-            {otherStories.map((d) => {
-              return (
-                <div key={d.id} className='grid-row'>
-                  <div className='tablet:grid-col'>
-                    <div
-                      className='card--homepage-substory text-base-lightest radius-md display-flex flex-align-end padding-2'
-                      style={{ backgroundImage: `url(${d.media?.src})` }}
-                    >
-                      <div className='card--homepage-topstory-text'>
-                        <h3> {d.name}</h3>
-                        <p className='margin-top-1'> {d.description}</p>
-                      </div>
-                      <Link className='link--block' href={d.path} />
+
+          <div className="tablet:grid-col-6">
+            {otherStories.map((d) => (
+              <div key={d.id} className="grid-row">
+                <div className="tablet:grid-col">
+                  <div
+                    className="card--homepage-substory text-base-lightest radius-md display-flex flex-align-end padding-2"
+                    style={{ backgroundImage: `url(${d.media?.src})` }}
+                  >
+                    <div className="card--homepage-topstory-text">
+                      <h3>{d.name}</h3>
+                      <p className="margin-top-1">{d.description}</p>
                     </div>
+                    <Link className="link--block" href={d.path} />
                   </div>
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
         </div>
       </div>
